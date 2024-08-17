c576a66b25dfb321a6d5d1503203086e4d4e9047
Andre Labendia <jaguardian00@gmail.com>
2024-08-17 14:47:00 +0800

    Added getUsersExceptUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\User;
 
 class User extends Authenticatable
 {
@@ -64,12 +65,18 @@ class User extends Authenticatable
             ->leftJoin('conversations', function ($join) use ($userId) {
                 $join->on('conversations.user_id1', '=', 'users.id')
                     ->where('conversations.user_id2', '=', $userId)
-                    ->orWhere(function ($query) {
+                    ->orWhere(function ($query) use ($userId){
                         $query->on('conversations.user_id2', '=', 'users.id')
-                            ->where('conversations.user_id1', '=', Auth::id());
+                            ->where('conversations.user_id1', '=', $userId);
                     });
             })
             ->leftJoin('messages', 'messages.id', '=', 'conversations.last_message_id')
+            ->orderByRaw('IFNULL(users.blocked_at, 1)')
+            ->orderBy('messages.created_at', 'desc')
+            ->orderBy('users.name')
             ;
+
+            // dd($query->toSql());
+            return $query->get();
     }
 }
