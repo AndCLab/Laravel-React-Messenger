5eeadb4f22f749e9d96c3cd5bbc88a3cabe24fc5
Andre Labendia <jaguardian00@gmail.com>
2024-08-21 22:39:28 +0800

    Utilized daisyui chat bubbles

--- resources/js/Components/App/MessageItem.jsx
+++ resources/js/Components/App/MessageItem.jsx
@@ -8,9 +8,8 @@ const MessageItem = ({ message }) => {
     const currentUser = usePage().props.auth.user;
 
     return (
-        <div 
-            className={"chat " + (message.sender_id === currentUser.id ? "chat-end" : "chat-start")}>
-            {<UserAvatar user={message.sender}/>}
+        <div className={`chat ${message.sender_id === currentUser.id ? "chat-end" : "chat-start"}`}>
+            <UserAvatar user={message.sender} />
             <div className="chat-header">
                 {message.sender_id !== currentUser.id ? message.sender.name : ""}
                 <time className="text-xs opacity-50 ml-2">
@@ -18,7 +17,7 @@ const MessageItem = ({ message }) => {
                 </time>
             </div>
 
-            <div className={"chat-bubble-relative " + (message.sender_id === currentUser.id ? " chat-bubble-info" : "")}>
+            <div className={`chat-bubble relative ${message.sender_id === currentUser.id ? "chat-bubble-info" : ""}`}>
                 <div className="chat-message">
                     <div className="chat-message-content">
                         <ReactMarkdown>{message.message}</ReactMarkdown>
