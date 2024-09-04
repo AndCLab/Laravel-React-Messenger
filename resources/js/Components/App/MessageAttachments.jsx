2d57d58734ab2ced2bd6e2faf1f9d477cb92f19c
Andre Labendia <jaguardian00@gmail.com>
2024-09-04 20:51:14 +0800

    Fixed MessageAttachments

--- resources/js/Components/App/MessageAttachments.jsx
+++ resources/js/Components/App/MessageAttachments.jsx
@@ -35,6 +35,32 @@ const MessageAttachments = ({ attachments, attachmentClick }) => {
                                     className="object-contain aspect-square"
                                 />
                             )}
+
+                            {isVideo(attachment) && (
+                                <div className="relative flex justify-center items-center">
+                                    <PaperClipIcon className="z-20 absolute w-16 h-16 text-white opacity-70" />
+                                    <div className="absolute left-0 top-0 w-full h-full bg-black/50 z-10">
+                                    </div>
+                                    <video src={attachment.url}></video>
+                                </div>
+                            )}
+                            {isAudio(attachment) && (
+                                <div className="relative flex justify-center items-center">
+                                    <audio
+                                        src={attachment.url}
+                                        controls
+                                    ></audio>
+                                </div>
+                            )}
+                            {isPDF(attachment) && (
+                                <div className="relative flex justify-center items-center">
+                                    <div className="absolute left-0 top-0 right-0 bottom-0"></div>
+                                    <iframe
+                                        src={attachment.url}
+                                        className="w-full h-full"    
+                                    ></iframe>
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
