c4ca637a1f0cdf56db3d8f12d54cd7aeb13e2146
Andre Labendia <jaguardian00@gmail.com>
2024-09-04 20:42:23 +0800

    Added isAudio render

--- resources/js/Components/App/MessageAttachments.jsx
+++ resources/js/Components/App/MessageAttachments.jsx
@@ -18,7 +18,16 @@ const MessageAttachments = ({ attachments, attachmentClick }) => {
                                 (isAudio(attachment) ? "w-84" : "w-32 aspect-square bg-blue-100")
                             }
                         >
+                            {!isAudio(attachment) && (
+                                <a
+                                    onClick={(ev) => ev.stopPropagation()}
+                                    download
+                                    href={attachment.url}
+                                    className="z-20 opacity-100 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-0 top-cursor-pointer hover:bg-gray-800"
+                                >
 
+                                </a>
+                            )}
                         </div>
                     ))}
                 </div>
